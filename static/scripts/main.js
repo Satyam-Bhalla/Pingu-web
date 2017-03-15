3b9a543f09759748408e29e6326fd6e15455e2bc
paras <parazz@outlook.com>
2017-03-16 00:22:08 +0530

    Design changes

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,11 +5,3 @@ function tab_login() {
 function tab_sign() {
     	$('ul.tabs').tabs('select_tab', 'signin');
 }
-
-$('textarea').each(function () {
-  this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
-}).on('input', function () {
-  this.style.height = 'auto';
-  this.style.height = (this.scrollHeight) + 'px';
-});
-  
